5eb76f2b0a03059560fed54e25e71bf03a88aa85
LeboHarmony <kebithil@gmail.com>
2019-07-04 16:36:28 +0200

    get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,15 @@
 /*   By: lkebethi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/30 10:55:45 by lkebethi          #+#    #+#             */
-/*   Updated: 2019/06/30 14:49:16 by lkebethi         ###   ########.fr       */
+/*   Updated: 2019/07/04 15:31:24 by lkebethi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32
-# include <unistd.h>
-# include <stdlib.h>
 # include "libft/libft.h"
+# include <fcntl.h>
 
 int		new_line(char **s, char **line, int fd, int ret);
 int		get_next_line(const int fd, char **line);
